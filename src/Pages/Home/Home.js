01b365c7e8701b66a324411431448d70c2f0b43f
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-11-12 15:17:20 +0800

    banner section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is a Home page.</h2>
+            <Banner />
         </div>
     );
 };
