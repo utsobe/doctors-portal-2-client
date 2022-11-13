16c538b231881aa8ce7a69837158f92af9f64091
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-11-13 20:13:32 +0800

    make appointment section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Banner from './Banner';
 import Info from './Info';
+import MakeAppointment from './MakeAppointment';
 import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner />
             <Info />
             <Services />
+            <MakeAppointment />
+            <Testimonials />
         </div>
     );
 };
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import doctor from '../../assets/images/doctor.png';
+import appointment from '../../assets/images/appointment.png';
+import PrimaryButton from '../Shared/PrimaryButton';
 
 const MakeAppointment = () => {
     return (
-        <div>
-
-        </div>
+        <section className='flex items-center justify-center' style={{
+            backgroundImage: `url('${appointment}')`
+        }}>
+            <div className='flex-1 hidden lg:block'>
+                <img className='mt-[-150px]' src={doctor} alt="" />
+            </div>
+            <div className='flex-1'>
+                <h3 className='text-xl text-primary font-bold'>Appointment</h3>
+                <h2 className='text-3xl text-white'>Make an appointment today</h2>
+                <p className='text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum alias temporibus quas repellat odio pariatur consectetur atque perferendis doloribus cupiditate quia consequuntur totam mollitia autem eos placeat nostrum earum iusto harum explicabo laboriosam, soluta illum consequatur adipisci! Quae, sint quam!</p>
+                <PrimaryButton>Get Started</PrimaryButton>
+            </div>
+        </section>
     );
 };
 
