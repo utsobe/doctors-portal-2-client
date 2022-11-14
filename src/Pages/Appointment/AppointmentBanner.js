63fbb5c3fe78b9545b2a1afac802fcc23958e7b2
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-11-15 05:53:19 +0800

    appointment banner added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import { format, getHours, getTime } from 'date-fns';
 
 const AppointmentBanner = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <DayPicker />
+                    <DayPicker
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                    />
+                    <p>You have selected date: {format(date, 'PP')}</p>
                 </div>
             </div>
         </div>
